6d2f964293416ffa55ebf9bad733da690d60efa3
sgopalan76 <47679208+sgopalan76@users.noreply.github.com>
2020-11-17 10:36:52 -0500

    Update CityConnectorApplication.java
    Added swagger as part of the change for documentation purposes

--- CityConnectorApplication.java
+++ CityConnectorApplication.java
@@ -3,7 +3,10 @@ package com.codechallenge.cityconnectservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CityConnectorApplication {
 
 	public static void main(String[] args) {
