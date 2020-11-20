dde3d492c4dd9f508c6da6bbb8d0fe62f598a9d7
sgopalan76 <47679208+sgopalan76@users.noreply.github.com>
2020-11-19 21:49:39 -0500

    Update CityConnectorApplicationTests.java
    Improved version with AppConfig

--- CityConnectorApplicationTests.java
+++ CityConnectorApplicationTests.java
@@ -1,64 +1,68 @@
 package com.codechallenge.cityconnect;
 
 import org.junit.jupiter.api.Test;
-import com.codechallenge.cityconnectservice.resource.CityConnectServiceResource;
+import org.junit.runner.RunWith;
+import com.codechallenge.cityconnectservice.util.AppConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 class CityConnectorApplicationTests {
-	
-	
+
 	@Test
-	  void cityConnectorRoutes_1() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("San Diego", "Los Angeles"));
-	  }
-	
+	void cityConnectorRoutes_1() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("San Diego", "Los Angeles"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_2() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("Los Angeles", "San Diego"));
-	  }
-	
+	void cityConnectorRoutes_2() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("Los Angeles", "San Diego"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_3() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("Boston", "New York"));
-	  }
-	
+	void cityConnectorRoutes_3() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("Boston", "New York"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_4() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("boston", "New york"));
-	  }
-	
+	void cityConnectorRoutes_4() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("boston", "New york"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_5() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("trenton", "albany"));
-	  }
-	
+	void cityConnectorRoutes_5() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("trenton", "albany"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_6() throws Exception {
-	    assertEquals("no",new CityConnectServiceResource().fetchCityConnectorRoutes("Delhi", "Mumbai"));
-	  }
-	
+	void cityConnectorRoutes_6() throws Exception {
+		assertEquals("no", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("Delhi", "Mumbai"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_7() throws Exception {
-	    assertEquals("no",new CityConnectServiceResource().fetchCityConnectorRoutes("Richmond", "DC"));
-	  }
-	
+	void cityConnectorRoutes_7() throws Exception {
+		assertEquals("no", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("Richmond", "DC"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_8() throws Exception {
-	    assertEquals("no",new CityConnectServiceResource().fetchCityConnectorRoutes("San Francisco", "Los Angeles"));
-	  }
-	
+	void cityConnectorRoutes_8() throws Exception {
+		assertEquals("no",
+				AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("San Francisco", "Los Angeles"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_9() throws Exception {
-	    assertEquals("yes",new CityConnectServiceResource().fetchCityConnectorRoutes("newark", "boston"));
-	  }
-	
+	void cityConnectorRoutes_9() throws Exception {
+		assertEquals("yes", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("newark", "boston"));
+	}
+
 	@Test
-	  void cityConnectorRoutes_10() throws Exception {
-	    assertEquals("no",new CityConnectServiceResource().fetchCityConnectorRoutes("", ""));
-	  }
-	
+	void cityConnectorRoutes_10() throws Exception {
+		assertEquals("no", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("", ""));
+	}
+
 	@Test
-	  void cityConnectorRoutes_11() throws Exception {
-	    assertEquals("no",new CityConnectServiceResource().fetchCityConnectorRoutes("", "Chennai"));
-	  }
+	void cityConnectorRoutes_11() throws Exception {
+		assertEquals("no", AppConfig.returnCityConnectorBean().fetchCityConnectorRoutes("", "Chennai"));
+	}
+
 }
